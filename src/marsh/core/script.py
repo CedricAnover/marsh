1cac19c1056b37cb19cac0c1226ec18a65638575
CedricAnover <cedric.anover@hotmail.com>
2024-12-24 16:02:22 +0800

    feat(script): add abstract base class for Script

--- src/marsh/core/script.py
+++ src/marsh/core/script.py
@@ -1,35 +1,38 @@
-import inspect
-import ast
-import re
-import string
+from typing import Any
 from abc import ABC, abstractmethod
 
 
-class ScriptStatement(ABC):
+class Script(ABC):
+    """
+    Abstract base class for generating scripts from templates. 
+    Subclasses must implement the `generate` method to produce 
+    a rendered script as a string.
+    """
+
+    def __init__(self, script_template: Any):
+        """
+        Initializes a Script instance with a template to be rendered.
+
+        Args:
+            script_template (Any): A template object (e.g., a `string.Template`, 
+                                   a Jinja2 template, or a plain string) 
+                                   that serves as the base for script generation.
+        """
+        self.script_template = script_template  # e.g. string.Template, Jinja2 Templates, or str
+
     @abstractmethod
-    def exec(self, *args, **kwargs) -> None:
+    def generate(self, *args, **kwargs) -> str:
+        """
+        Abstract method to render the script template into a string.
+
+        This method should be implemented in subclasses to provide specific logic
+        for rendering the `script_template` using the provided arguments.
+
+        Args:
+            *args: Positional arguments to customize the script rendering.
+            **kwargs: Keyword arguments to customize the script rendering.
+
+        Returns:
+            str: A string containing the rendered script, ready to be executed or used.
+        """
         pass
-
-
-class ScriptExpression:
-    ...
-
-
-class Script:
-    # Properties:
-    # -----------
-    # template: string.Template | str
-    # shebang: str = "#!/usr/bin/env bash"
-    # statements: list[str] = []
-    # debugging: str | list[str] = "set -eu -o pipefail"
-    # stmt_sep: str = "\n\n"
-    # ...
-    # ...
-
-    # Methods:
-    # --------
-    # render(*args, **kwargs) -> str
-    # ...
-    # ...
-    # ...
-    ...
